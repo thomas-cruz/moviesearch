f8888caaf98c4f1dfec261cc688838c4d90092fe
thomas-cruz <tom.ortega47@gmail.com>
2019-05-06 12:38:57 +0800

    Initial commit, set up for kraggle dataset and reactiveSearch

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { ReactiveBase } from '@appbaseio/reactivesearch';
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="main-container">
+        <ReactiveBase
+          app="MovieAppFinal"
+          credentials="RxIAbH9Jc:6d3a5016-5e9d-448f-bd2b-63c80b401484"
+          theme={{
+            typography: {
+              fontFamily:
+                '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Noto Sans", "Ubuntu", "Droid Sans", "Helvetica Neue", sans-serif',
+              fontSize: "16px"
+            },
+            colors: {
+              textColor: "#fff",
+              backgroundColor: "#212121",
+              primaryTextColor: "#fff",
+              primaryColor: "#2196F3",
+              titleColor: "#fff",
+              alertColor: "#d9534f",
+              borderColor: "#666"
+            }
+          }}
+         >
+          Hello from ReactiveSearch!
+        </ReactiveBase>
+      </div>
+    );
+  }
+}
+export default Main;
